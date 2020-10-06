6c2dd823b2fab6ef34d4655fe13af271db5a0c85
Georgi <garios_97@abv.bg>
2020-10-06 11:12:48 +0300

    Final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the coordinates: ");
         int n = sc.nextInt();
         int m = sc.nextInt();
+        while(n<1 || n>3 || m<1 || m>3){
+            System.out.println("Invalid input, try again: ");
+            n = sc.nextInt();
+            m = sc.nextInt();
+        }
+
         Tile[][] desk = new Tile[4][4];
         Board board = new Board(desk);
         Game tryOne = new Game(board);
